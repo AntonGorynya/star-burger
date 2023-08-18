c0d3095b1b54fe714f0058db2f8535cb181b022c
Anton <antongorynya1@gmail.com>
2023-08-18 15:55:52 +0300

    add exceptions

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -44,21 +44,24 @@ class OrderSerializer(ModelSerializer):
 
 
 def fetch_coordinates(apikey, address):
-    base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
-    })
-    response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+    try:
+        base_url = "https://geocode-maps.yandex.ru/1.x"
+        response = requests.get(base_url, params={
+            "geocode": address,
+            "apikey": apikey,
+            "format": "json",
+        })
+        response.raise_for_status()
+        found_places = response.json()['response']['GeoObjectCollection']['featureMember']
 
-    if not found_places:
+        if not found_places:
+            return {'lon': None, 'lat': None}
+
+        most_relevant = found_places[0]
+        lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+        return {'lon': lon, 'lat': lat}
+    except requests.exceptions.RequestException:
         return {'lon': None, 'lat': None}
-
-    most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-    return {'lon': lon, 'lat': lat}
 
 
 def banners_list_api(request):
